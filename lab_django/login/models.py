08d6d4b099b58ad255f24addecc5014db89f87e8
THY <iamthy@mail.ustc.edu.cn>
2020-05-04 09:10:29 +0800

    update .gitignore

--- lab_django/login/models.py
+++ lab_django/login/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    name = models.CharField(max_length=128,unique=True)#用于登录的用户名，必须唯一
+    password = models.CharField(max_length=256)#密码
+    
