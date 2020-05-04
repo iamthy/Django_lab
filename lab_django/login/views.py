fe33892ac64dbf09a742d567c918dfd18fcd2401
THY <iamthy@mail.ustc.edu.cn>
2020-05-04 11:23:00 +0800

    增加了登录错误时的提示信息

--- lab_django/login/views.py
+++ lab_django/login/views.py
@@ -17,9 +17,15 @@ def login(request):
             try:
                 user = models.User.objects.get(name=username)#从数据库中根据用户名检索
             except:
-                return render(request, 'login/login.html')#没有这个用户就返回登录界面
+                message = '用户名不正确！'
+                return render(request, 'login/login.html', {'message': message})#没有这个用户就返回登录界面
             if (user.password == password): # 有的话还要检查密码是否正确
                 return redirect('/index/')
+            else:
+                message = '密码不正确！'
+                return render(request, 'login/login.html', {'message': message})
+        else:
+            return render(request, 'login/login.html', {'message': message})
     return render(request, 'login/login.html')
 
 
